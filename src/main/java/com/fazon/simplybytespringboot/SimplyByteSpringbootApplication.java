dc22a378db0c95dafc4c2d726b66a78d325b86fe
Simply Byte <umarfariznew1266@gmail.com>
2025-08-17 13:52:08 +0530

    Update SimplyByteSpringbootApplication.java

--- src/main/java/com/fazon/simplybytespringboot/SimplyByteSpringbootApplication.java
+++ src/main/java/com/fazon/simplybytespringboot/SimplyByteSpringbootApplication.java
@@ -3,7 +3,11 @@ package com.fazon.simplybytespringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class
+})
 public class SimplyByteSpringbootApplication {
 
     public static void main(String[] args) {
